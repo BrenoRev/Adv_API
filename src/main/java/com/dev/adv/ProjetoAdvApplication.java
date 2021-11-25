bbac93a8dde4cb1a325190eeaa615cc9fd7f9c84
BrenoRev <silvabreno462@gmail.com>
2021-11-25 12:37:53 -0300

    Starting project

---
+++ src/main/java/com/dev/adv/ProjetoAdvApplication.java
@@ -0,0 +1,13 @@
+package com.dev.adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAdvApplication.class, args);
+	}
+
+}
